2abd4bf83342666c05ee7f66a8dc1f61a442de2a
psalmotee <samsonmoradeyo@gmail.com>
2025-08-05 17:49:46 +0100

    Update vite.config.ts to improve build configuration and optimize performance

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/School-Employee-Management-System",
+  base: "/School-Employee-Management-System/",
 });
